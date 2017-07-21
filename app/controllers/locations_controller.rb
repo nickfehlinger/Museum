7302fb4e260b402c472ca810ebe5ef0ee806d804
Neil Parsa <parsank@miamioh.edu>
2017-07-21 16:37:03 -0400

    css for locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,12 +4,11 @@ class LocationsController < ApplicationController
 
   def create
   end
-<<<<<<< HEAD
-=======
+
 
   def show
-  	@location = Location.find_by(params[:id])
+  	@location = Location.find_by(id: params[:id])
   	@pieces = @location.pieces.all
   end
->>>>>>> 68109f39247bd4cdf3856ef55e3397995a3af0b1
+
 end
