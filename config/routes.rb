ccf21f218e8f2ac55723208efdb68d44ba0bcfae
Neil Parsa <parsank@miamioh.edu>
2017-07-24 16:19:06 -0400

    added fname, lname, phone number

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, controllers: {sessions: 'user/sessions'}
+  devise_for :users, :controllers => { registrations: 'user/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
